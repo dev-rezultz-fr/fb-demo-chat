25848d0ca1c64bdedef4f969a02a499c3c362a47
dev-rezultz-fr <dev@rezultz.fr>
2020-11-10 18:29:46 +0100

    Codelabs, step 7

--- main.js
+++ main.js
@@ -1,86 +1,49 @@
-// Import stylesheets
-//import "./style.css";
-// Firebase App (the core Firebase SDK) is always required and must be listed first
-import * as firebase from "firebase/app";
-
-// Add the Firebase products that you want to use
-import "firebase/auth";
-import "firebase/storage";
-import "firebase/messaging";
-import "firebase/firestore";
-import "firebase/performance";
-
-//import * as firebaseui from "firebaseui";
-
-// Document elements
-const messageListElement = document.getElementById("messages");
-const messageFormElement = document.getElementById("message-form");
-const messageInputElement = document.getElementById("message");
-const submitButtonElement = document.getElementById("submit");
-const imageButtonElement = document.getElementById("submitImage");
-const imageFormElement = document.getElementById("image-form");
-const mediaCaptureElement = document.getElementById("mediaCapture");
-const userPicElement = document.getElementById("user-pic");
-const userNameElement = document.getElementById("user-name");
-const signInButtonElement = document.getElementById("sign-in");
-const signOutButtonElement = document.getElementById("sign-out");
-const signInSnackbarElement = document.getElementById("must-signin-snackbar");
-
-// Add Firebase project configuration object here
-const firebaseConfig = {
-  apiKey: "AIzaSyDD8-Ii5QbbcBwDQvGQrL4nX6LdVdI2Wvc",
-  authDomain: "fb-demo-e35f2.firebaseapp.com",
-  databaseURL: "https://fb-demo-e35f2.firebaseio.com",
-  projectId: "fb-demo-e35f2",
-  storageBucket: "fb-demo-e35f2.appspot.com",
-  messagingSenderId: "1026081219114",
-  appId: "1:1026081219114:web:330294a128a04dc0d89953"
-};
-
-firebase.initializeApp(firebaseConfig);
+/**
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+"use strict";
 
 // Signs-in Friendly Chat.
 function signIn() {
-  // Sign into Firebase using popup auth & Google as the identity provider.
-  var provider = new firebase.auth.GoogleAuthProvider();
-  firebase
-    .auth()
-    .signInWithPopup(provider)
-    .then(function(result) {
-      console.log(result);
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
+  alert("TODO: Implement Google Sign-In");
+  // TODO 1: Sign in Firebase with credential from the Google user.
 }
 
 // Signs-out of Friendly Chat.
 function signOut() {
-  // Sign out of Firebase.
-  firebase.auth().signOut();
+  // TODO 2: Sign out of Firebase.
 }
 
-// Initiate Firebase Auth.
+// Initiate firebase auth.
 function initFirebaseAuth() {
-  // Listen to auth state changes.
-  firebase.auth().onAuthStateChanged(authStateObserver);
+  // TODO 3: Initialize Firebase.
 }
 
-// Returns the signed-in user's profile pic URL.
+// Returns the signed-in user's profile Pic URL.
 function getProfilePicUrl() {
-  return (
-    firebase.auth().currentUser.photoURL || "/images/profile_placeholder.png"
-  );
+  // TODO 4: Return the user's profile pic URL.
 }
 
 // Returns the signed-in user's display name.
 function getUserName() {
-  return firebase.auth().currentUser.displayName;
+  // TODO 5: Return the user's display name.
 }
 
 // Returns true if a user is signed-in.
 function isUserSignedIn() {
-  return !!firebase.auth().currentUser;
+  // TODO 6: Return true if a user is signed-in.
 }
 
 // Saves a new message on the Firebase DB.
@@ -142,41 +105,6 @@ function onMessageFormSubmit(e) {
       resetMaterialTextfield(messageInputElement);
       toggleButton();
     });
-  }
-}
-
-// Triggers when the auth state change for instance when the user signs-in or signs-out.
-function authStateObserver(user) {
-  if (user) {
-    // User is signed in!
-    // Get the signed-in user's profile pic and name.
-    var profilePicUrl = getProfilePicUrl();
-    var userName = getUserName();
-
-    // Set the user's profile pic and name.
-    userPicElement.style.backgroundImage =
-      "url(" + addSizeToGoogleProfilePic(profilePicUrl) + ")";
-    userNameElement.textContent = userName;
-
-    // Show user's profile and sign-out button.
-    userNameElement.removeAttribute("hidden");
-    userPicElement.removeAttribute("hidden");
-    signOutButtonElement.removeAttribute("hidden");
-
-    // Hide sign-in button.
-    signInButtonElement.setAttribute("hidden", "true");
-
-    // We save the Firebase Messaging Device token and enable notifications.
-    saveMessagingDeviceToken();
-  } else {
-    // User is signed out!
-    // Hide user's profile and sign-out button.
-    userNameElement.setAttribute("hidden", "true");
-    userPicElement.setAttribute("hidden", "true");
-    signOutButtonElement.setAttribute("hidden", "true");
-
-    // Show sign-in button.
-    signInButtonElement.removeAttribute("hidden");
   }
 }
 
@@ -317,11 +245,6 @@ function toggleButton() {
   }
 }
 
-// Saves message on form submit.
-messageFormElement.addEventListener("submit", onMessageFormSubmit);
-signOutButtonElement.addEventListener("click", signOut);
-signInButtonElement.addEventListener("click", signIn);
-
 // Toggle for the button.
 messageInputElement.addEventListener("keyup", toggleButton);
 messageInputElement.addEventListener("change", toggleButton);
@@ -333,10 +256,3 @@ imageButtonElement.addEventListener("click", function(e) {
 });
 mediaCaptureElement.addEventListener("change", onMediaFileSelected);
 
-// initialize Firebase
-initFirebaseAuth();
-
-// TODO: Enable Firebase Performance Monitoring.
-
-// We load currently existing chat messages and listen to new ones.
-loadMessages();
